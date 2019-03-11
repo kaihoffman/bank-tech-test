bb2043441a606e291e2a9583c01246329212b3d7
Kai <notkaiho@gmail.com>
2019-03-11 12:01:41 +0000

    Feature tests spec added with user stories

---
+++ spec/features/features_spec.rb
@@ -0,0 +1,48 @@
+require 'bank-account'
+
+describe "Bank Account feature tests" do
+
+
+# ```
+# As an account holder
+# In order to have money in my account
+# I want to be able to deposit money.
+# ```
+
+# ```
+# As an account holder
+# In order to later know when money was deposited or withdrawn,
+# I want to be able to input a date for my operation
+# ```
+
+# ```
+# As an account holder,
+# In order to have access to my money
+# I want to be able to withdraw money.
+# ```
+
+# ```
+# As an account holder,
+# Because money comes in decimals,
+# I would like to be able to withdraw, deposit and view my balance with 2 decimals
+# ```
+
+# ```
+# As an account holder,
+# In order to not go over my funds
+# I want to only be able to withdraw what is available in my account
+# ```
+
+# ```
+# As an account holder,
+# So that I know how much money is in my account
+# I want to be able to request a balance statement
+# ```
+
+# ```
+# As an account holder,
+# So that I can follow my funds over time
+# I want to be able to see my statements ordered by date
+# ```
+
+end
